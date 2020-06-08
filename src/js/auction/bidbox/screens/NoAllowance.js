e7593a6af66de25d28d5e330c7e02ae22abe453e
Bernd Bohmeier <berndbohmeier@googlemail.com>
2020-06-08 18:33:38 +0200

    Fix No Allowance text

--- src/js/auction/bidbox/screens/NoAllowance.js
+++ src/js/auction/bidbox/screens/NoAllowance.js
@@ -10,11 +10,9 @@ export default function NoAllowance({ web3Account, approve }) {
     <Screen faIcon="fa fa-arrow-circle-right" title="Approve Transfer">
       <MessageBlock>
         To proceed, you must approve the transfer of{" "}
-        <TLNLink>Trustlines Network Tokens</TLNLink> on your behalf by the
+        <TLNLink>Trustlines Network Tokens</TLNLink> on your behalf by the{" "}
         <AuctionLink>auction contract</AuctionLink>. You are currently using the
-        address {web3Account} to approve the transfer. In order to proceed,
-        please approve the transfer of <TLNLink>TLN</TLNLink> by the for{" "}
-        {web3Account}.
+        address {web3Account} to approve the transfer.
       </MessageBlock>
       <ActionButton label="Approve" onClick={approve} />
     </Screen>
