835281f66b3602e814d9149cd954099f6be60cdf
Bernd Bohmeier <berndbohmeier@googlemail.com>
2020-06-22 14:16:48 +0200

    Small text fix in auction screen

--- src/js/auction/bidbox/screens/SuccessfulWithdraw.js
+++ src/js/auction/bidbox/screens/SuccessfulWithdraw.js
@@ -1,14 +1,13 @@
 import React from "react"
 import MessageBlock from "../components/MessageBlock"
 import Screen from "./Screen"
-import { formatTLNAmount } from "../../../common/math"
 import * as blockexplorer from "../../../common/blockexplorer"
 
 export default function SuccessfulWithdraw({ txHash }) {
   return (
     <Screen faIcon="fa fa-check-circle" title="You have successfully withdrawn">
       <MessageBlock>
-        You successfully withdrawn your overbid from the auction
+        You have successfully withdrawn your overbid from the auction
         <br />
         You can check your transaction on{" "}
         <a
