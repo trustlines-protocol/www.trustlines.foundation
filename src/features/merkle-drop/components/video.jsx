af6db2c76453ecaf3b04505a070fc25572184a3a
nekhaly <nekhaly@gmail.com>
2021-09-21 15:21:19 +0200

    Adjust video size for different screen sizes

--- src/features/merkle-drop/components/video.jsx
+++ src/features/merkle-drop/components/video.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import YouTube from "react-youtube";
 
 import { Button } from "../../common/components/button";
 import { Play } from "../../common/components/icons/play";
+import useWindowDimensions from "../../common/hooks/window";
 
 export function MerkleDropVideo() {
   const [play, setPlay] = useState(false);
@@ -12,33 +13,28 @@ export function MerkleDropVideo() {
   };
 
   const opts = {
-    height: "390",
-    width: "640",
+    height: "720",
+    width: "1280",
     playerVars: {
       autoplay: 1,
     },
   };
+  const { width } = useWindowDimensions();
+
+  const adjustedWidth = width <= 1280 ? width : 1280;
+  opts.width = Math.floor(adjustedWidth * 0.9);
+  opts.height = Math.floor((opts.width * 9) / 16);
 
   return (
     <section className="bg-rich-black">
       <div className="flex flex-col items-center justify-center bg-dark-green-lighter">
-        <div className="flex flex-col items-center justify-center video-container">
+        <div className="flex flex-col items-center justify-center min-w-max">
           {play ? (
             <div>
               <YouTube videoId="bHLYpZstZKs" opts={opts} />
-              {/* <iframe
-                id="player"
-                width="560"
-                height="315"
-                src="https://www.youtube.com/embed/bHLYpZstZKs"
-                title="YouTube video player"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe> */}
             </div>
           ) : (
-            <div>
+            <div className="flex justify-center items-center video-container">
               <Button onClick={handlePlay}>
                 <Play />
               </Button>
