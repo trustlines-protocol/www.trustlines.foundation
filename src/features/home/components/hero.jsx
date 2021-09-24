42dbe70f58fd1558ae4046db0c9b15f5d199e39f
nekhaly <nekhaly@gmail.com>
2021-09-24 09:21:53 +0200

    Adjust home screen for mobile

--- src/features/home/components/hero.jsx
+++ src/features/home/components/hero.jsx
@@ -7,16 +7,24 @@ import worldMap from "../images/world-map.svg";
 export function HomeHero() {
   return (
     <section className="container mx-auto px-4 mb-32">
-      <div className="hero-header items-center">
-        <h1 className="text-6xl font-semibold leading-tight">
-          Financial
-          <br className="lg:hidden" /> Inclusion for all.
-          <br /> Leveraging trust.
-        </h1>
-        <img src={worldMap} alt="world map" />
-      </div>
-      <div className="flex flex-row">
-        <LinkButton href="/" label="Read about the Trustlines Foundation →" />
+      <div className="grid grid-col-1">
+        <div className="hero-header items-center">
+          <div className="relative container mx-auto flex flex-row justify-end">
+            <div className="absolute top-20 left-0 px-4">
+              <h1 className="text-4xl md:text-6xl font-semibold leading-tight">
+                Financial
+                <br /> Inclusion for all.
+                <br /> Leveraging trust.
+              </h1>
+            </div>
+            <div className="py-24 md:py-0 opacity-70 -z-1">
+              <img src={worldMap} alt="world map" />
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row">
+          <LinkButton href="/" label="Read about the Trustlines Foundation →" />
+        </div>
       </div>
     </section>
   );
--- src/features/home/components/subscribe.jsx
+++ src/features/home/components/subscribe.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { Mail } from "../../common/components/icons/mail";
 import { Check } from "../../common/components/icons/check";
 
 import { isEmailValid } from "../../common/utils/validation";
 
 import mail from "../images/mail.svg";
+import { MailDot } from "../../common/components/icons/mail-dot";
 
 export function HomeSubscribe() {
   const [didSubscribe, setDidSubscribe] = React.useState(false);
@@ -14,7 +14,7 @@ export function HomeSubscribe() {
   const submitUrl = process.env.REACT_APP_MAILCHIMP_URL;
 
   return (
-    <section className="container mx-auto flex flex-row justify-center pt-16 text-rich-black-lightest">
+    <section className="md:container md:mx-auto md:pb-0 pb-20 flex flex-row justify-center pt-16 text-rich-black-lightest">
       <img src={mail} alt="mail" />
       {didSubscribe ? (
         <div className="absolute pt-40 max-w-sm mx-auto flex flex-col">
@@ -28,7 +28,7 @@ export function HomeSubscribe() {
         </div>
       ) : (
         <form
-          className="absolute pt-40 max-w-sm mx-auto flex flex-col"
+          className="absolute md:pt-40 md:p-0 p-10 pt-28 max-w-sm mx-auto flex flex-col"
           action={submitUrl}
           method="post"
           target="_blank"
@@ -36,14 +36,14 @@ export function HomeSubscribe() {
             setDidSubscribe(true);
           }}
         >
-          <Mail />
-          <h1 className="text-2xl font-semibold mt-4">
+          <MailDot />
+          <h1 className="md:text-2xl text-xl font-semibold mt-4">
             Be the first
             <br /> to get updates
           </h1>
           <div className="flex flex-row rounded-full bg-dark-green text-white h-14 my-4">
             <input
-              className="text-lg bg-dark-green w-full ml-6 px-2 placeholder-off-white"
+              className="md:text-lg bg-dark-green w-full ml-6 px-2 placeholder-off-white"
               type="email"
               name="EMAIL"
               value={email}
@@ -61,7 +61,7 @@ export function HomeSubscribe() {
               </button>
             </div>
           </div>
-          <div className="text-sm text-rich-black-lightest">
+          <div className="md:text-sm text-xs text-rich-black-lightest">
             By submitting your email address you acknowledge that your
             information will be transferred to Mailchimp for processing and
             agree to our{" "}
