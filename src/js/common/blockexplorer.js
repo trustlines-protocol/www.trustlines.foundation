267085b793e1d9595db9c8e2ec27ea96f1d5dc91
nekhaly <nekhaly@gmail.com>
2021-10-13 11:10:54 +0200

    Remove old website scripts

--- src/js/common/blockexplorer.js
+++
@@ -1,3 +0,0 @@
-export function generateTransactionUrl(tx_hash) {
-  return `${process.env.REACT_APP_EXPLORER_URL}tx/${tx_hash}`
-}
--- src/js/newsletter/components/Appeal.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Appeal(props) {
-  return (
-    <p {...props} className={`title is-5 ${props.className}`}>
-      {props.children}
-    </p>
-  )
-}
