74e3958b069a4b502bea0ba2f7bf699255d4f42c
nekhaly <nekhaly@gmail.com>
2021-10-20 15:22:37 +0200

    Move Contact hero a little bit down
    To match with other screen headers.

--- src/features/contact/components/hero.jsx
+++ src/features/contact/components/hero.jsx
@@ -4,7 +4,7 @@ import ContactForm from "./contact-form";
 
 export function ContactHero() {
   return (
-    <section className="md:container mx-auto px-4 mb-10 md:mb-32 text-rich-black-lighter md:px-12">
+    <section className="md:container mx-auto px-4 mb-10 md:mb-32 md:pt-12 text-rich-black-lighter md:px-12">
       <div className="hero-header">
         <div className="flex-1">
           <h1 className="md:text-6xl text-4xl font-semibold leading-tight mb-10 text-rich-black pt-6">
